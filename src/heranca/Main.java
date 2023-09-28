c3db54cbeecfa572a5470c4804b2f9e594bc549c
Estevan Sousa <estevanmartins03@gmail.com>
2023-09-28 15:51:29 -0300

    :recycle:feat:giving a intuitive name to the counter of array index

--- src/heranca/Main.java
+++ src/heranca/Main.java
@@ -5,25 +5,21 @@ public class Main {
 
         Figura[] figuras = new Figura[150];
         double soma = 0;
-        int j = 0;
+        int contador_index = 0;
 
-        for (int i = 1; j < figuras.length; i++) {
+        for (int i = 1; contador_index < figuras.length; i++) {
             //cria os objetos passando o valor de i
             Circulo c = new Circulo(0,0,i);
             Retangulo r = new Retangulo(0,0,i,i);
             Triangulo tr = new Triangulo(0,0,i,i,i);
 
-            double areaC = c.getArea();
-            double areaR = r.getArea();
-            double areaT = tr.getArea();
-
             //soma das areas
             soma += (c.getArea() + r.getArea() + tr.getArea());
 
             //vetor com todos os objetos
-            figuras[j++] = c;
-            figuras[j++] = r;
-            figuras[j++] = tr;
+            figuras[contador_index++] = c;
+            figuras[contador_index++] = r;
+            figuras[contador_index++] = tr;
         }
         String soma_formato = String.format("%.2f", soma);
         System.out.println("Valor da soma das areas: " + soma_formato);
